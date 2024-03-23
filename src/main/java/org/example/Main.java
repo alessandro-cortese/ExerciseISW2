b8685c62727244040236135def60b6b895e5df6d
Alessandro Cortese <ale.cortese99@gmail.com>
2024-03-23 15:53:30 +0100

    Added Code
    Added code for sum of two integer numbers.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int A = 4;
+        int B = 6;
+        out.println("Sum of 4 and 6: " + Sum.sum(A, B));
+    }
+}

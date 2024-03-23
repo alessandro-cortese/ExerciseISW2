41ad6d69e7edeac3654e0fcdb28518832b6f8919
Alessandro Cortese <ale.cortese99@gmail.com>
2024-03-23 15:59:00 +0100

    Modify Sum
    Sum of two real number.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,9 @@ public class Main {
         int A = 4;
         int B = 6;
         out.println("Sum of 4 and 6: " + Sum.sum(A, B));
+
+        float C = 4.5F;
+        float D = 5.5F;
+        out.println("Sum of 4.5 and 5.5: " + Sum.sum(C, D));
     }
 }
--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -4,4 +4,8 @@ public class Sum {
     public static int sum(int a, int b){
         return a+b;
     }
+
+    public static float sum(float a, float b){
+        return a+b;
+    }
 }
